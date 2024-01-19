a5a5b5e42179d135e87ba4dd134914b96191ce60
nescafe <dev89@protonmail.com>
2024-01-20 00:05:36 +1100

    Tidy of code, finalized grids

--- generate_template.py
+++ generate_template.py
@@ -17,10 +17,15 @@ class SVGCreator:
         self.divs_n = divs_n
         self.divs_m = divs_m
 
-
         self.stroke_width = stroke_width
         self.stroke_color = stroke_color
         self.background_color = background_color
+
+    def from_mm(self, input_tuple):
+        return (f"{input_tuple[0]}mm", f"{input_tuple[1]}mm")
+    
+    
+
 
     def create_svg_with_rectangle(self, filename, canvas_size_mm):
         """
@@ -32,27 +37,15 @@ class SVGCreator:
         :param square_position_mm: tuple, the top-left position of the square in millimeters (x, y).
         """
         # Convert sizes and position to mm
-        canvas_size = (f"{canvas_size_mm[0]}mm", f"{canvas_size_mm[1]}mm")
-
+        canvas_size = self.from_mm(canvas_size_mm)
 
         # Create outer rectangle
-        rectangle_size_x_o = canvas_size_mm[0]-(2*self.page_padding_mm)
-        rectangle_size_y_o = canvas_size_mm[1]-(2*self.page_padding_mm)
-        rectangle_size_o = (f"{rectangle_size_x_o}mm", f"{rectangle_size_y_o}mm")
-
-        x_position_o = (canvas_size_mm[0] - rectangle_size_x_o) / 2
-        y_position_o = (canvas_size_mm[1] - rectangle_size_y_o) / 2
-        rectangle_position_o = (f"{x_position_o}mm", f"{y_position_o}mm")
+        outer_rec_size_mm = tuple(x-(2*self.page_padding_mm) for x in canvas_size_mm)
+        outer_rec_pos_mm = tuple(((a - b)/2.0) for a, b in zip(canvas_size_mm, outer_rec_size_mm))
         
         # Create inner rectangle
-        rectangle_size_x_i = canvas_size_mm[0]-2*(self.page_padding_mm+self.border_thk_mm)
-        rectangle_size_y_i = canvas_size_mm[1]-2*(self.page_padding_mm+self.border_thk_mm)
-        rectangle_size_i = (f"{rectangle_size_x_i}mm", f"{rectangle_size_y_i}mm")
-
-        x_position_i = (canvas_size_mm[0] - rectangle_size_x_i) / 2
-        y_position_i = (canvas_size_mm[1] - rectangle_size_y_i) / 2
-        rectangle_position_i = (f"{x_position_i}mm", f"{y_position_i}mm")
-
+        inner_rec_size_mm =tuple(x-2*(self.page_padding_mm+self.border_thk_mm) for x in canvas_size_mm)
+        inner_rec_pos_mm = tuple(((a - b)/2.0) for a, b in zip(canvas_size_mm, inner_rec_size_mm))
 
         # Create an SVG drawing instance with the specified canvas size in mm
         dwg = svgwrite.Drawing(filename, size=canvas_size, profile='tiny')
@@ -63,8 +56,8 @@ class SVGCreator:
 
         # Add a square to the drawing with no fill, using class attributes for stroke
         square = dwg.rect(
-            insert=rectangle_position_o, 
-            size=rectangle_size_o, 
+            insert=self.from_mm(outer_rec_pos_mm), 
+            size=self.from_mm(outer_rec_size_mm), 
             fill='none',
             stroke=self.stroke_color,
             stroke_width=self.stroke_width
@@ -72,54 +65,101 @@ class SVGCreator:
         dwg.add(square)
         
         inner_square = dwg.rect(
-            insert=rectangle_position_i, 
-            size=rectangle_size_i, 
+            insert=self.from_mm(inner_rec_pos_mm), 
+            size=self.from_mm(inner_rec_size_mm), 
             fill='none',
             stroke=self.stroke_color,
             stroke_width=self.stroke_width
         )
         dwg.add(inner_square)        
 
+        # X grid
+        start_x, start_y = (outer_rec_pos_mm[0], canvas_size_mm[1]/2)
+        start_position = self.from_mm((start_x,start_y)) 
+        start_position_opp = self.from_mm((canvas_size_mm[0]-start_x,start_y)) 
 
-        # Convert start position and length to mm
-        start_x, start_y = (self.page_padding_mm, canvas_size_mm[1]/2)
-        start_position = (f"{start_x}mm", f"{start_y}mm")
-        end_position = (f"{start_x + self.border_thk_mm}mm", f"{start_y}mm")
-
+        end_position = self.from_mm((start_x+ self.border_thk_mm,start_y)) 
+        end_position_opp = self.from_mm((canvas_size_mm[0]-(start_x+ self.border_thk_mm),start_y)) 
 
         # Create the line with the specified properties
-        line = dwg.line(
-            start=start_position,
-            end=end_position,
-            stroke=self.stroke_color,
-            stroke_width=self.stroke_width
-        )
-        dwg.add(line)
+        dwg.add(dwg.line(start=start_position,end=end_position,stroke=self.stroke_color,stroke_width=self.stroke_width*2))
+        dwg.add(dwg.line(start=start_position_opp,end=end_position_opp,stroke=self.stroke_color,stroke_width=self.stroke_width*2))
+
+
         for i in range(self.divs_n-1):
-            line = dwg.line(
-                start=(f"{start_x}mm", f"{ start_y+(i+1)*((rectangle_size_y_i)/2)/(self.divs_n)}mm"),
-                end=(f"{start_x + self.border_thk_mm}mm", f"{start_y+(i+1) * ((rectangle_size_y_i)/2)/(self.divs_n)}mm"),
-                stroke=self.stroke_color,
-                stroke_width=self.stroke_width
-            )
-            dwg.add(line)
+            pitch_y = (i+1)*((inner_rec_size_mm[1])/2)/(self.divs_n)
+
+            dwg.add(dwg.line(
+                start=self.from_mm((start_x, start_y+pitch_y)),
+                end=self.from_mm((start_x+self.border_thk_mm, start_y+pitch_y)),
+                stroke=self.stroke_color, stroke_width=self.stroke_width
+            ))
             
-            line = dwg.line(
-                start=(f"{start_x}mm", f"{ start_y-(i+1) *((rectangle_size_y_i)/2)/(self.divs_n)}mm"),
-                end=(f"{start_x + self.border_thk_mm}mm", f"{start_y-(i+1) * ((rectangle_size_y_i)/2)/(self.divs_n)}mm"),
-                stroke=self.stroke_color,
-                stroke_width=self.stroke_width
-            )
-            dwg.add(line)
+            dwg.add(dwg.line(
+                start= self.from_mm((start_x, start_y-pitch_y)),
+                end=self.from_mm((start_x + self.border_thk_mm, start_y-pitch_y)),
+                stroke=self.stroke_color, stroke_width=self.stroke_width
+            ) )
+
+            dwg.add(dwg.line(
+                start=self.from_mm((canvas_size_mm[0]-(start_x + self.border_thk_mm), start_y+pitch_y)),
+                end=self.from_mm((canvas_size_mm[0]-(start_x), start_y+pitch_y)),
+                stroke=self.stroke_color, stroke_width=self.stroke_width
+            ))
+            
+            dwg.add(dwg.line(
+                start=self.from_mm((canvas_size_mm[0]-(start_x + self.border_thk_mm), start_y-pitch_y)),
+                end=self.from_mm((canvas_size_mm[0]-(start_x), start_y-pitch_y)),
+                stroke=self.stroke_color, stroke_width=self.stroke_width
+            ))
+
+             # Y grid
+        start_x, start_y = (canvas_size_mm[0]/2, outer_rec_pos_mm[1])
+
+        start_position = self.from_mm((start_x,start_y)) 
+        start_position_opp = self.from_mm((start_x,canvas_size_mm[1]-start_y)) 
+
+        end_position = self.from_mm((start_x,start_y+ self.border_thk_mm)) 
+        end_position_opp = self.from_mm((start_x,canvas_size_mm[1]-(start_y+ self.border_thk_mm))) 
+
+        # Create the line with the specified properties
+        dwg.add(dwg.line(start=start_position,end=end_position,stroke=self.stroke_color,stroke_width=self.stroke_width*2))
+        dwg.add(dwg.line(start=start_position_opp,end=end_position_opp,stroke=self.stroke_color,stroke_width=self.stroke_width*2))
 
 
-        # Save the drawing as an SVG file
+        for i in range(self.divs_m-1):
+            pitch_x = (i+1)*((inner_rec_size_mm[0])/2)/(self.divs_m)
+
+            dwg.add(dwg.line(
+                start=self.from_mm((start_x+pitch_x, start_y)),
+                end=self.from_mm((start_x+pitch_x, start_y+self.border_thk_mm)),
+                stroke=self.stroke_color, stroke_width=self.stroke_width
+            ))
+            
+            dwg.add(dwg.line(
+                start= self.from_mm((start_x-pitch_x, start_y)),
+                end=self.from_mm((start_x-pitch_x, start_y+self.border_thk_mm)),
+                stroke=self.stroke_color, stroke_width=self.stroke_width
+            ) )
+
+            dwg.add(dwg.line(
+                start=self.from_mm((start_x + pitch_x, canvas_size_mm[1]-(start_y+self.border_thk_mm))),
+                end=self.from_mm((start_x + pitch_x, canvas_size_mm[1]-start_y)),
+                stroke=self.stroke_color, stroke_width=self.stroke_width
+            ))
+            
+            dwg.add(dwg.line(
+                start=self.from_mm((start_x - pitch_x, canvas_size_mm[1]-(start_y+self.border_thk_mm))),
+                end=self.from_mm((start_x - pitch_x, canvas_size_mm[1]-start_y)),
+                stroke=self.stroke_color, stroke_width=self.stroke_width
+            ))
+
+        # Save the drawing as awr   n SVG file
         dwg.save()
 
-# Usage Example
-svg_creator = SVGCreator(2, 'white', 50, 30)
+
+svg_creator = SVGCreator(3, 'white', 50, 30)
 
 svg_creator.create_svg_with_rectangle('square_with_background.svg', Specs.A1.value[::-1])
 
-# print(Specs.A4)  # (210, 297)
 
